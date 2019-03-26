29ad66724b71be1fea9885479286e146b686b222
Bogumił Kamiński <bkamins@sgh.waw.pl>
2019-03-26 23:39:57 +0100

    Allow Julia 0.6

--- 6. Sudoku/sudoku2.jl
+++ 6. Sudoku/sudoku2.jl
@@ -1,14 +1,21 @@
 # Bogumił Kamiński, 2019-03-25
+# a drop-in replacement of sudoku_solve from sudoku1.jl
+
+# can also work under Julia 0.6, JuMP 0.18.5 and Cbc 0.4.3
 
 using LinearAlgebra
 using JuMP
 using Cbc
 
-# a drop-in replacement of sudoku_solve from sudoku1.jl
+const JULIA1 = VERSION >= v"1.0"
 
 function sudoku_solve(lines, idx)
     t = [lines[10idx-j][k] - '0' for j in 8:-1:0, k in 1:9]
-    m = Model(with_optimizer(Cbc.Optimizer, logLevel=0))
+    m = if JULIA1
+            Model(with_optimizer(Cbc.Optimizer, logLevel=0))
+        else
+            Model(solver=CbcSolver())
+        end
     @variable(m, x[1:9, 1:9, 1:9], Bin)
     for i in 1:9, j in 1:9
         @constraint(m, sum(x[i, j, :]) == 1)
@@ -19,10 +26,9 @@ function sudoku_solve(lines, idx)
     for i in 0:2, j in 0:2, k in 1:9
         @constraint(m, sum(x[3i .+ (1:3), 3j .+ (1:3), k]) == 1)
     end
-    optimize!(m)
-    xv = value.(x)
+    JULIA1 ? optimize!(m) : solve(m)
+    xv = JULIA1 ? value.(x) : getvalue(x)
     v = 100dot(1:9, xv[1,1,:]) + 10dot(1:9, xv[1,2,:]) + dot(1:9, xv[1,3,:])
     println(lines[10idx-9], ":\t", v)
     return v
 end
-
